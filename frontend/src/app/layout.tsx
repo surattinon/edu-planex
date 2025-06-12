630e5a9bacc0b86f9a3ed4d8725dbfd29a117ba3
surattinon <surattinon.basu@gmail.com>
2025-06-12 18:48:00 +0700

    Init: Recreate frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "@/components/ui/theme-provider";
-import { SessionNavBar } from "@/components/ui/sidebar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,18 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-        >
-          <SessionNavBar />
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
